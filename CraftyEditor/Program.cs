688570cb04e2fc2e2f7c46e1dfe9341337ecc35e
tangi3 <tangi3.business@gmail.com>
2021-02-13 21:10:43 +0100

    cleaning project to migrate to full open gl structure

---
+++ CraftyEditor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CraftyEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
