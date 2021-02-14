16a155e72c98ddec7c29334100ca94f19edcf3c4
tangi3 <tangi3.business@gmail.com>
2021-02-14 15:59:09 +0100

    opengl drawing working !

--- CraftyEditor/Editor/Ogame/Ogame.cpp
+++ CraftyEditor/Editor/Ogame/Ogame.cpp
@@ -5,7 +5,9 @@ using namespace std;
 class Ogame
 {
 	private: string title;
-	private: int width, height;
+
+	private: int width, height, editorViewX, editorViewY;
+	private: float ratio;
 
 	private: GLFWwindow* window;
 
@@ -15,6 +17,9 @@ class Ogame
 
 		width = screen_width;
 		height = screen_height;
+
+		editorViewX = 0;
+		editorViewY = 0;
 	}
 
 	public: void Run()
@@ -32,24 +37,21 @@ class Ogame
 
 	private: int Initialize()
 	{
-		if (!glfwInit())
-		{
-			cout << "OpenGL couldn't initialize\n" << endl;
-			return -1;
-		}
+		if (!glfwInit()) return -1;
 
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		//glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		//glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 		window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+		//setViewport();
 		
 		if (!window)
 		{
-			cout << "OpenGL couldn't create a window\n" << endl;
-
 			glfwTerminate();
 			return -1;
 		}
+
+		glfwMakeContextCurrent(window);
 
 		Init();
 	}
@@ -71,7 +73,7 @@ class Ogame
 
 	private: void Render()
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
+		Clear();
 
 		glfwSwapBuffers(window);
 
@@ -80,12 +82,28 @@ class Ogame
 		Draw();
 	}
 
+	private: void Clear(int r = -1, int g = 0, int b = 0, int alpha = 255)
+	{
+		if(r > -1) glClearColor(r, g, b, alpha);
+		else glClear(GL_COLOR_BUFFER_BIT);
+	}
+
 	private: int Exit()
 	{
-		cout << "OpenGL closed safely" << endl;
-
 		glfwTerminate();
 
 		return 0;
+	}
+
+	private: void setViewport()
+	{
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+		glViewport(editorViewX, editorViewY, width, height);
+	}
+
+	private: void debug()
+	{
+		//...
 	}
 };
