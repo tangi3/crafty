39abbfdce1c10cc7960f95d11fd23821661625fe
tangi3 <tangi3.business@gmail.com>
2021-02-20 17:15:49 +0100

    moving project to sfml

--- CraftyEditor/Main.cpp
+++ CraftyEditor/Main.cpp
@@ -1,7 +1,29 @@
-#include "Editor/Game.cpp"
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
 
-int main(void)
+int main()
 {
-    Game game = Game("Editor", 1400, 800);
-    game.run();
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    auto desktop = sf::VideoMode::getDesktopMode();
+    window.setPosition(Vector2i(desktop.width / 2 - window.getSize().x / 2, desktop.height / 2 - window.getSize().y / 2));
+
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
